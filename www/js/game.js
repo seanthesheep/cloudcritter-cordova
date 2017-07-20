c8dca5470b63d2bcdbecb84a2474b17e499494cb
Sean Sullivan <ssullivandev@gmail.com>
2017-07-19 19:56:39 -0500

    added states

--- www/js/game.js
+++ www/js/game.js
@@ -1,15 +1,12 @@
 var game = new Phaser.Game(window.innerWidth, window.innerHeight, Phaser.AUTO, 'game'), Game = function() {};
 
 
-Game.prototype = {
-create: function() {
+
+
 game.state.add('load', loadState);
 game.state.add('menu', menuState);
 game.state.add('play', play);
-game.state.start('menu');
-}
-    
-};
-game.state.add('Game', Game);
-game.state.start('Game');
+
+
+game.state.start('load');
     

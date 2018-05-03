e3b1cddd9b92b74d8ba45847b21c44d9f881a545
sean sullivan <ssullivandev@gmail.com>
2018-05-02 20:01:45 -0500

    added admob plugin and broke the build

---
+++ platforms/android/app/src/main/assets/www/js/load.js
@@ -0,0 +1,21 @@
+		var loadState = {
+            
+            preload:function(){
+           // this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;this.scale.minWidth = 360;this.scale.minHeight = 680;this.scale.maxWidth = 768;this.scale.maxHeight = 1152;
+                 
+           
+            game.load.script("webfont", "//ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js");
+            game.add.text(0, 0, "fix", {font:"1px VT323", fill:"#2979FF"});
+			game.load.spritesheet("critter", "images/critter.png", 16,16);
+			game.load.image("pole", "images/cloud.png");
+            game.load.image("powerbar", "images/powerbar.png");
+            game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+            game.load.audio("jumping", "audio/jumping.wav");
+            game.load.audio("landing", "audio/landing.wav");
+           
+		},
+            create:function(){
+                game.state.start('menu');
+            }
+        };
+        
